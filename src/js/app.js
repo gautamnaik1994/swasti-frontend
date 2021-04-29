72e8fd3542219d86b348bb955fc78891099ed378
Sanket Nainani <sanket.drake@gmail.com>
2021-04-29 19:53:46 +0530

    adding accordian

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,32 @@
+window.addEventListener("load", function () {
+  var inputs = document.querySelectorAll(".input-file");
+  console.log(inputs);
+  Array.prototype.forEach.call(inputs, function (input) {
+    var label = input.nextElementSibling,
+      labelVal = label.innerHTML;
 
-window.addEventListener('load', function() {
-var inputs = document.querySelectorAll( '.input-file' );
-console.log(inputs)
-Array.prototype.forEach.call( inputs, function( input )
-{
-	var label	 = input.nextElementSibling,
-		labelVal = label.innerHTML;
+    input.addEventListener("change", function (e) {
+      var fileName = "";
+      fileName = e.target.value.split("\\").pop();
 
-	input.addEventListener( 'change', function( e )
-	{
-		var fileName = '';
-		fileName = e.target.value.split( '\\' ).pop();
-
-		if( fileName )
-			label.innerHTML = fileName;
-		else
-			label.innerHTML = labelVal;
-	});
+      if (fileName) label.innerHTML = fileName;
+      else label.innerHTML = labelVal;
+    });
+  });
+  console.log("All assets are loaded");
 });
-    console.log('All assets are loaded')
-})
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
